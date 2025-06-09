dfd4b66e8d82f7739196c7b1eea3a4846a1691de
bhavini1105 <bhavinipatel7532@gmail.com>
2025-06-09 10:27:27 +0530

    Portfolio

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -16,22 +16,22 @@ function Education() {
       <div className="education-card">
         <small>2024 - Present</small>
         <h5><i className="bi bi-person-fill" />Full Stack Development</h5>
-        <p className="mb-0">Red And White Institute </p>
+        <p className="mb-0 text-white">Red And White Institute </p>
       </div>
       <div className="education-card">
         <small>2018 - 2022</small>
         <h5><i className="bi bi-person-fill" />Bachelor Degree in Computer Engeneering</h5>
-        <p className="mb-0">S S Agrawal Institute </p>
+        <p className="mb-0 text-white">S S Agrawal Institute </p>
       </div>
       <div className="education-card">
         <small>2016 - 2018</small>
         <h5><i className="bi bi-person-fill" />Higher Secondary</h5>
-        <p className="mb-0">A B School Navsari</p>
+        <p className="mb-0 text-white">A B School Navsari</p>
       </div>
       <div className="education-card">
         <small>2014 - 2016</small>
         <h5><i className="bi bi-person-fill" />Secondary School School</h5>
-        <p className="mb-0">A B School Navsari</p>
+        <p className="mb-0 text-white">A B School Navsari</p>
       </div>
     </div>
   </div>
